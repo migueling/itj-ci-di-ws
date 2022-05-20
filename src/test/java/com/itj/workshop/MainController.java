d85b6aead4826c3ed41d311e9def091424630c1b
Miguel Sanchez <ing.miguels@gmail.com>
2022-05-20 17:36:17 -0500

    Test 2

--- src/test/java/com/itj/workshop/MainController.java
+++ src/test/java/com/itj/workshop/MainController.java
@@ -22,7 +22,7 @@ public class MainController {
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hola Mundo")));
+                .andExpect(content().string(containsString("Hola Mundo 2")));
     }
 
 }
