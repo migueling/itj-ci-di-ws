50a973a0ae0a041a5bfd1b96970a617f200a665a
Miguel Sanchez <ing.miguels@gmail.com>
2022-05-20 17:01:41 -0500

    Added hello world message

---
+++ src/main/java/com/itj/workshop/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.itj.workshop.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hola Mundo";
+    }
+}
